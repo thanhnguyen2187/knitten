1552269ebcdd50545e9566bb21117842afbfe6b1
Thanh Nguyen <thanhnguyen2187@gmail.com>
2023-10-30 18:43:57 +0700

    Split to `create_tables` and `seed_data`

--- create_tables.sql
+++ create_tables.sql
@@ -5,17 +5,3 @@ CREATE TABLE products (
     patterns TEXT NOT NULL,
     image_url TEXT NOT NULL
 );
-
-INSERT INTO products VALUES ('a84918f3-dfde-4d46-952f-616b40608a7a', 'Product Name 1',  'Product Description 1',  'Patterns 1',  'https://http.cat/images/100.jpg');
-INSERT INTO products VALUES ('4d2a6030-79d1-40b3-99ab-6427431f51e1', 'Product Name 2',  'Product Description 2',  'Patterns 2',  'https://http.cat/images/101.jpg');
-INSERT INTO products VALUES ('aac70325-f4b1-4921-8af0-f1146558495a', 'Product Name 3',  'Product Description 3',  'Patterns 3',  'https://http.cat/images/102.jpg');
-INSERT INTO products VALUES ('b13efd18-b9a6-4791-9968-29c6559639d9', 'Product Name 4',  'Product Description 4',  'Patterns 4',  'https://http.cat/images/103.jpg');
-INSERT INTO products VALUES ('1826a9d6-6476-4ca6-9395-134343dce43e', 'Product Name 5',  'Product Description 5',  'Patterns 5',  'https://http.cat/images/200.jpg');
-INSERT INTO products VALUES ('a2c557ea-6512-46df-84d9-f910c4b022c6', 'Product Name 6',  'Product Description 6',  'Patterns 6',  'https://http.cat/images/201.jpg');
-INSERT INTO products VALUES ('598e1bcd-b0d7-46b0-b200-b1248b9a52f5', 'Product Name 7',  'Product Description 7',  'Patterns 7',  'https://http.cat/images/202.jpg');
-INSERT INTO products VALUES ('05b096bf-771b-4678-9ffa-aeb87f660bb0', 'Product Name 8',  'Product Description 8',  'Patterns 8',  'https://http.cat/images/203.jpg');
-INSERT INTO products VALUES ('e6fbedce-70e7-434f-ba7d-3505f76eb459', 'Product Name 9',  'Product Description 9',  'Patterns 9',  'https://http.cat/images/204.jpg');
-INSERT INTO products VALUES ('1f1e60ee-fbce-4d92-87a5-d7c14132a005', 'Product Name 10', 'Product Description 10', 'Patterns 10', 'https://http.cat/images/205.jpg');
-INSERT INTO products VALUES ('c1db516a-7e5f-4a36-9c71-305fa0cf2ba7', 'Product Name 11', 'Product Description 11', 'Patterns 11', 'https://http.cat/images/206.jpg');
-INSERT INTO products VALUES ('ed3d5f52-35ca-4c41-adbc-6fc26f99e248', 'Product Name 12', 'Product Description 12', 'Patterns 12', 'https://http.cat/images/207.jpg');
-INSERT INTO products VALUES ('2cf3f3c2-21db-4926-ad25-16cebca9f3bd', 'Product Name 13', 'Product Description 13', 'Patterns 13', 'https://http.cat/images/300.jpg');
