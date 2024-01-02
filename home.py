88408c694943e48d438788424abedebd00a2daaf
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-01-02 21:05:28 +0700

    Make home page redirect to sign up page if not logged in

--- home.py
+++ home.py
@@ -1,10 +1,19 @@
 from nicegui import ui
+from fastapi.responses import RedirectResponse
 
 import components
+import global_state
+
+
+def f():
+    ui.open(target="/sign-up")
 
 
 @ui.page("/")
 def page():
+    if not global_state.dict_["logged_in_user"]:
+        return RedirectResponse('/sign-up')
+
     ui.label(text="Knitten 🐱 - Yarn Knitting Management").classes(add="text-xl")
 
     components.search_bar()
